f0d260ffff2b0e7d6228426d9fb3caf182f24368
koketsomaake <koketsomaake95@gmail.com>
2020-05-12 19:58:17 +0200

    third commit

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -1,10 +1,9 @@
 package com.project;
 
-import Intefaces.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class ProjectApplication {
@@ -12,6 +11,5 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-@Bean
-public void  demo(UserService userService){userService.addUser("koketso","maake");}
+
 }
